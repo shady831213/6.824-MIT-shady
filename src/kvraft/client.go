a64e41a97341d23462f15574fa97330bdd149b06
Ubuntu <shady831213>
2019-09-26 03:26:07 -0700

    3A reliable pass

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -4,10 +4,10 @@ import "labrpc"
 import "crypto/rand"
 import "math/big"
 
-
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	leader int
 }
 
 func nrand() int64 {
@@ -21,6 +21,7 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.leader = 0
 	return ck
 }
 
@@ -36,10 +37,34 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 //
-func (ck *Clerk) Get(key string) string {
+func (ck *Clerk) updateLeader() {
+	if ck.leader == len(ck.servers)-1 {
+		ck.leader = 0
+	} else {
+		ck.leader ++
+	}
+}
 
+func (ck *Clerk) leadServer() *labrpc.ClientEnd {
+	return ck.servers[ck.leader]
+}
+
+func (ck *Clerk) Get(key string) string {
+	args := GetArgs{key}
+	var reply *GetReply
+	var ok bool
+	req := func() {
+		reply = &GetReply{}
+		ok = ck.leadServer().Call("KVServer.Get", &args, reply)
+	}
+	for req(); !ok || reply.WrongLeader; req() {
+		ck.updateLeader()
+	}
+	if reply.Err != OK {
+		return (string)(reply.Err)
+	}
 	// You will have to modify this function.
-	return ""
+	return reply.Value
 }
 
 //
@@ -54,6 +79,15 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	args := PutAppendArgs{key, value, op}
+	req := func() (bool, PutAppendReply) {
+		reply := PutAppendReply{}
+		ok := ck.leadServer().Call("KVServer.PutAppend", &args, &reply)
+		return ok, reply
+	}
+	for ok, reply := req(); !ok || reply.WrongLeader; ok, reply = req() {
+		ck.updateLeader()
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -1,6 +1,7 @@
 package raftkv
 
 import (
+	"context"
 	"labgob"
 	"labrpc"
 	"log"
@@ -17,11 +18,28 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type OPCode string
+
+const (
+	GET    = "Get"
+	PUT    = "Put"
+	APPEND = "Append"
+)
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	OpCode OPCode
+	Id     int
+	Key    string
+	Value  string
+}
+
+type KVCommit struct {
+	WrongLeader bool
+	Err         Err
+	op          *Op
 }
 
 type KVServer struct {
@@ -33,15 +51,93 @@ type KVServer struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	db       map[string]string
+	ctx      context.Context
+	cancel   func()
+	commit   chan KVCommit
+	curIndex int
 }
-
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	op := Op{GET, kv.me, args.Key, ""}
+	index, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	kv.curIndex = index
+	commit := <-kv.commit
+	reply.Err = commit.Err
+	reply.WrongLeader = commit.WrongLeader
+	reply.Value = commit.op.Value
+	DPrintf("reply Get me: %d %+v %+v", kv.me, args, reply)
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	op := Op{(OPCode)(args.Op), kv.me, args.Key, args.Value}
+	index, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	kv.curIndex = index
+	commit := <-kv.commit
+	reply.Err = commit.Err
+	reply.WrongLeader = commit.WrongLeader
+	DPrintf("reply PutAppend me: %d %+v %+v", kv.me, args, reply)
+}
+
+func (kv *KVServer) execute(op *Op) Err {
+	switch op.OpCode {
+	case PUT:
+		kv.db[op.Key] = op.Value
+		break
+	case GET:
+		v, exist := kv.db[op.Key]
+		if !exist {
+			return ErrNoKey
+		}
+		op.Value = v
+		break
+	case APPEND:
+
+		if v, exist := kv.db[op.Key]; !exist {
+			kv.db[op.Key] = op.Value
+		} else {
+			kv.db[op.Key] = v + op.Value
+		}
+		DPrintf("Execute APPEND %s, %s", op.Key, kv.db[op.Key])
+		break
+	}
+	return OK
+}
+
+func (kv *KVServer) applyMap() {
+	for {
+		select {
+		case apply := <-kv.applyCh:
+			if apply.CommandValid {
+				op, _ := (apply.Command).(Op)
+				err := kv.execute(&op)
+				if kv.curIndex == apply.CommandIndex {
+					kv.commit <- KVCommit{
+						op.Id != kv.me,
+						err,
+						&op,
+					}
+				}
+			}
+			break
+		case <-kv.ctx.Done():
+			return
+		}
+	}
 }
 
 //
@@ -53,6 +149,7 @@ func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 func (kv *KVServer) Kill() {
 	kv.rf.Kill()
 	// Your code here, if desired.
+	kv.cancel()
 }
 
 //
@@ -79,11 +176,15 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxraftstate = maxraftstate
 
 	// You may need initialization code here.
+	kv.db = make(map[string]string)
+	kv.commit = make(chan KVCommit)
+	kv.ctx, kv.cancel = context.WithCancel(context.Background())
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	go kv.applyMap()
 
 	return kv
 }
